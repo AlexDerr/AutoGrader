adf5db1dd08fbdffce9428314b791d58eb1b23e0
KhoiNg <nguyenphantankhoi@gmail.com>
2018-09-04 13:56:46 -0500

    Add Model for Assignment

---
+++ AutoGrader/AutoGrader/Models/Assignment/Assignment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+
+namespace Assignment
+{ 
+    public class Assignment
+    {
+	    public Assignment()
+	    {
+            InputFileNames = new List<string>();
+            OutputFileNames = new List<string>();
+            Feedbacks = new List<string>();
+            Languages = new List<string>();
+	    }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> InputFileNames { get; set; }
+
+        public List<string> OutputFileNames { get; set; }
+
+        public List<string> Feedbacks { get; set; }
+
+        public int MemoryLimit { get; set; }
+
+        public int TimeLimit { get; set; }
+
+        public List<string> Languages { get; set; }
+    }
+}
+
+
