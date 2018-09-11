cb12dcedf15d64e0860fc291fc8ef2e490baf66a
AlexDerr <alex.derr999@gmail.com>
2018-09-11 14:34:03 -0500

    Adding comments to describe the process in which the grader will operate

--- AutoGrader/AutoGrader/Methods/Grader/Grader.cs
+++ AutoGrader/AutoGrader/Methods/Grader/Grader.cs
@@ -18,10 +18,19 @@ namespace AutoGrader.Methods.Grader
             // report.ReportId = next available id in database
             report.AssignmentId = submission.AssignmentId;
             report.SubmissionTime = submission.SubmissionTime;
-            report.Language = submission.Language; // May need to change
+            report.Language = submission.Language; // May need to change depending how we represent language
             report.SourceCodeFileName = submission.SourceCodeFileName;
 
+            // Send the submission to the compiler and get the output back
+            // Object returned with contain a pass or fail bool, and compiler output
+            
+            // if it did not compile correctly, make the output the error message
+            //      Mark all test cases on the report as failed
+            // else
+            //      call the grading function passing an object that contains ** 2d array of input output files/or assignment ID **, source code, language, runtime, and submission id
+            //      get back a list of objects in the same amount as the num of test cases, that each contain pass/fail bool, compiler/runner output, and runtime 
+            //      Update the database for the report ID to say which test cases were passed and save the output string for each
 
-        }   
+        }
     }
 }
