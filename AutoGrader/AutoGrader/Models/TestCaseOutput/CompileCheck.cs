40f5043fc752d191002c5e755e082fce783e7395
AlexDerr <alex.derr999@gmail.com>
2018-09-11 14:44:37 -0500

    Added a class that will be returned from linux to check if the program correctly compiled or not

---
+++ AutoGrader/AutoGrader/Models/TestCaseOutput/CompileCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoGrader.Models.TestCaseOutput
+{
+    public class CompileCheck
+    {
+        public CompileCheck()
+        {
+        }
+
+        public bool Compiled { get; set; }
+
+        public string CompilerOutput { get; set; }
+    }
+}
