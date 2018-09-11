571af0fbffa5fd22fae5905ced411b0baebda9df
AlexDerr <alex.derr999@gmail.com>
2018-09-11 14:38:52 -0500

    Added TestCaseReport class that will be used to return information about each run of a test case

---
+++ AutoGrader/AutoGrader/Models/TestCaseReport/TestCaseReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoGrader.Models.TestCaseReport
+{
+    public class TestCaseReport
+    {
+        public TestCaseReport()
+        {
+        }
+
+        public bool PassFail { get; set; }
+
+        public string CompilerOutput { get; set; }
+
+        public int Runtime { get; set; }
+    }
+}
