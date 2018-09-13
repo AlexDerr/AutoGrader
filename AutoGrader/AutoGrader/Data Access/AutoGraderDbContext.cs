4f7d880d4e48ef34dcebacfee6b5d604c8399fbd
looloosha <a.davidyuk@yahoo.com>
2018-09-13 15:05:52 -0500

    Initial EFCoreSetup
    Initial EFCore Setup

---
+++ AutoGrader/AutoGrader/Data Access/AutoGraderDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoGrader.Models.Assignment;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoGrader.DataAccess
+{
+    public class AutoGraderDbContext : DbContext
+    {
+        public AutoGraderDbContext(DbContextOptions<AutoGraderDbContext> contextOptions) : base(contextOptions) { }
+
+        public DbSet<Assignment> Assignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- AutoGrader/AutoGrader/Startup.cs
+++ AutoGrader/AutoGrader/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoGrader.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +35,8 @@ namespace AutoGrader
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AutoGraderDbContext>(contextOptions => contextOptions.UseNpgsql(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
