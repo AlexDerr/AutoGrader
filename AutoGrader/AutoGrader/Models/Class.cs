9a39678c28900d257c36132cbb1ff2c3e30cceff
maxcruse <crusemax@gmail.com>
2018-09-25 13:19:00 -0500

    Added a Class model.

---
+++ AutoGrader/AutoGrader/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using AutoGrader.Models.Users;
+using System.Collections.Generic;
+using AutoGrader.Models.Assignment;
+
+namespace AutoGrader.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Instructor Instructor { get; set; }
+        public List<Assignment.Assignment> Assignments { get; set; }
+    }
+}
