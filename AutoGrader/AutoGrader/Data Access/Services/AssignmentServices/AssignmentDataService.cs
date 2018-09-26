bd2ac220d8d5c1eb79be45fbeafe1de2f9899a03
looloosha <a.davidyuk@yahoo.com>
2018-09-26 14:21:26 -0500

    Added assignmentDataService

--- AutoGrader/AutoGrader/Data Access/Services/AssignmentServices/AssignmentDataService.cs
+++ AutoGrader/AutoGrader/Data Access/Services/AssignmentServices/AssignmentDataService.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoGrader.Models.Assignment;
+
 namespace AutoGrader.DataAccess
 {
     public class AssignmentDataService : Service
     {
         public AssignmentDataService(AutoGraderDbContext dbContext) : base(dbContext) { }
+
+        public IEnumerable<Assignment> GetAssignments()
+        {
+            return autoGraderDbContext.Assignments.ToList();
+        }
+
+        public Assignment GetSubmissionInputById(int id)
+        {
+            return GetAssignments().FirstOrDefault(e => e.Id == id);
+        }
     }
 }
