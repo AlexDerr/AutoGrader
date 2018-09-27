90f01406ddde0bd65def0afbf99e8a3d60140182
AlexDerr <alex.derr999@gmail.com>
2018-09-27 13:50:23 -0500

    Updating the grader method to remove error causing code and reflect the changes in the refactored classes

--- AutoGrader/AutoGrader/Methods/GraderMethod/GraderMethod.cs
+++ AutoGrader/AutoGrader/Methods/GraderMethod/GraderMethod.cs
@@ -9,30 +9,20 @@ namespace AutoGrader.Methods.GraderMethod
 {
     public class GraderMethod
     {
-        public void GradeSubmission(Assignment assignment, SubmissionInput submission)
+        public void GradeSubmission(Assignment assignment, Submission submission)
         {
-            SubmissionOutput report = new SubmissionOutput();
-
-            report.UserId = submission.UserId;
-            report.SubmissionInputId = submission.SubmissionInputId;
-            // report.ReportId = next available id in database
-            report.AssignmentId = submission.AssignmentId;
-            report.SubmissionTime = submission.SubmissionTime;
-            report.Language = submission.Language; // May need to change depending how we represent language
-            report.SourceCodeFileName = submission.SourceCodeFileName;
-
-            CompileCheckInput compileInput = new CompileCheckInput();
-            compileInput.SubmissionId = report.SubmissionInputId;
-            // Send the CompileCheckInput to the compiler and get the output back
+            // Send the Submission to the compiler and get the output back
             // Object returned with contain a pass or fail bool, and compiler output
             
             // if it did not compile correctly, make the output the error message
             //      Print out compiler output
-            //      Mark all test cases on the report as failed
+            //      Mark all test cases on the report as failed in submissionOutput
             // else
-            //      call the grading function passing an object that contains ** 2d array of input output files/or assignment ID **, source code, language, runtime, and submission id
-            //      get back a list of objects in the same amount as the num of test cases, that each contain pass/fail bool, compiler/runner output, and runtime 
-            //      Update the database for the report ID to say which test cases were passed and save the output string for each
+            //      call the grading function passing the submission, ** 2d array of input output files/or assignment ID **, source code, language, runtime, and submission id
+            //      get back the submission with the chekced fields updated
+            
+            // Update the database for the report ID to say which test cases were passed and save the output string for each
+            // return submission / submission output to the user
         }
     }
 }
