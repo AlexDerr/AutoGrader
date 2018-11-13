1a06e66d73d8f1e77f86b41cef654fa115e93a9b
AlexDerr <alex.derr999@gmail.com>
2018-11-13 13:44:08 -0600

    Adjusting the class class to add a constructor and modify fields

--- AutoGrader/AutoGrader/Models/Class.cs
+++ AutoGrader/AutoGrader/Models/Class.cs
@@ -1,14 +1,29 @@
 ﻿using AutoGrader.Models.Users;
 using System.Collections.Generic;
 using AutoGrader.Models.Assignment;
+using AutoGrader.Models.ViewModels;
 
 namespace AutoGrader.Models
 {
     public class Class
     {
+        public Class()
+        {
+            Assignments = new List<Assignment.Assignment>();
+        }
+
+        public Class(ClassViewModel model)
+        {
+            Assignments = new List<Assignment.Assignment>();
+
+            this.Name = model.Name;
+            this.ClassKey = model.ClassKey;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public Instructor Instructor { get; set; }
+        public string ClassKey { get; set; }
+        public int InstructorId { get; set; }
         public List<Assignment.Assignment> Assignments { get; set; }
     }
 }
