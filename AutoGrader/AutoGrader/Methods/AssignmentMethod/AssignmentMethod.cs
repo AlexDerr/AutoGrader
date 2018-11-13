32501e81950dd083ff0a759151787564083dc1f1
KhoiNg <nguyenphantankhoi@gmail.com>
2018-11-13 13:31:46 -0600

    Update Grader and Assignment Method

--- AutoGrader/AutoGrader/Methods/AssignmentMethod/AssignmentMethod.cs
+++ AutoGrader/AutoGrader/Methods/AssignmentMethod/AssignmentMethod.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoGrader.DataAccess;
 using AutoGrader.Models.Assignment;
 
 namespace AutoGrader.Methods.AssignmentMethod
 {
     public class AssignmentMethod
     {
-        public void UpdateAssignment(Assignment assignment)
+        public void UpdateAssignment(Assignment assignment, AutoGraderDbContext dbContext)
         {
             // Figure out assignemnt id
             // Call service to update assignment to database
@@ -17,12 +18,24 @@ namespace AutoGrader.Methods.AssignmentMethod
             //       Call method to update/add test cases to the database
         }
 
-        public List<Assignment> GetAssignments(int InstructorID)
+        public List<Assignment> GetAssignmentsForInstructor(int InstructorID, AutoGraderDbContext dbContext)
         {
-            List<Assignment> assignments = new List<Assignment>();
+            return null;
+//            AssignmentDataService assignmentDataService = new AssignmentDataService(dbContext);
+//            IEnumerable<Assignment> assignments = assignmentDataService.GetAssignmentById(InstructorID);
             // for all instructor assignments
             //      add each to a list of assignments
-            return assignments;
+//            return assignments.ToList();
+        }
+
+        public List<Assignment> GetAssignmentsForStudent(int StudentID, AutoGraderDbContext dbContext)
+        {
+            return null;
+            //            AssignmentDataService assignmentDataService = new AssignmentDataService(dbContext);
+            //            IEnumerable<Assignment> assignments = assignmentDataService.GetAssignmentById(StudentID);
+            // for all instructor assignments
+            //      add each to a list of assignments
+            //            return assignments.ToList();
         }
     }
 }
