911f516c156ca52cd92fe99a9846e9bce64cc19d
maxcruse <crusemax@gmail.com>
2018-11-20 14:21:14 -0600

    Update to the student controller to get class flow working. not there yet

--- AutoGrader/AutoGrader/Controllers/StudentController.cs
+++ AutoGrader/AutoGrader/Controllers/StudentController.cs
@@ -1,5 +1,9 @@
 ﻿using AutoGrader.DataAccess;
+using AutoGrader.DataAccess.Services.ClassServices;
+using AutoGrader.Models;
 using AutoGrader.Models.Assignment;
+using AutoGrader.Models.Users;
+using AutoGrader.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,12 +21,34 @@ namespace AutoGrader.Controllers
             this.dbContext = dbContext;
         }
 
-        public IActionResult StudentHome()
+        public IActionResult StudentHome(User user)
         {
             AssignmentDataService assignmentDataService = new AssignmentDataService(dbContext);
             IEnumerable<Assignment> assignments = assignmentDataService.GetAssignments();
 
+            //IEnumerable<Assignment> assignments = assignmentDataService.GetAssignmentsByUserId(Int32.Parse(user.Id));
+
             return View(assignments);
+        }
+
+        public IActionResult JoinClass()
+        {
+            //ClassDataService classDataService = new ClassDataService(dbContext);
+            //IEnumerable<Class> classes = classDataService.GetClasses();
+
+            //return View(classes);
+            return PartialView("JoinClass", new JoinClassViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> JoinClass(string ClassKey)
+        {
+            ClassDataService classDataService = new ClassDataService(dbContext);
+
+
+            await dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
