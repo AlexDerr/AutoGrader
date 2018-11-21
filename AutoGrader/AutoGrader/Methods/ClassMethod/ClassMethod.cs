c52e875ea36d00f9195fe6df9f143a6201402850
KhoiNg <nguyenphantankhoi@gmail.com>
2018-11-20 23:24:34 -0600

    Add Class Method

---
+++ AutoGrader/AutoGrader/Methods/ClassMethod/ClassMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoGrader.Methods.ClassMethod
+{
+    public class ClassMethod
+    {
+        public static string GenerateUniqueKey()
+        {
+            return Guid.NewGuid().ToString().Substring(0,5) + DateTime.Now.ToString("MMddyyyyHHmmss");
+        }
+    }
+}
